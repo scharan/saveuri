41015b4cdd83872016644c7016039369ad8f7e52
Sai Charan Koduru <me@saicharan.in>
2018-11-06 17:47:32 -0800

    Cleanup

--- saveuri.js
+++ saveuri.js
@@ -7,7 +7,6 @@ var anchor = document.createElement('a');
 anchor.id = "saveuri.anchor.id";
 anchor.href = uri;
 anchor.text = uri;
-// anchor.style = "color: white; visited-color: white;";
 
 div.appendChild(anchor);
 document.body.appendChild(div);
